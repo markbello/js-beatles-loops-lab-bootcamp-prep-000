caff00492157ced05400b50c44a50878c1494183
Mark Bello <mcbello51286@gmail.com>
2017-11-16 22:37:17 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
 function theBeatlesPlay(musicians, instruments){
   var arr = [];
   for(var i = 0; i< musicians.length; i++){
-    arr.push(`${musicians[i]} plays ${instruments[i]}`);
+    var str = `${musicians[i]} plays ${instruments[i]}`;
+    arr.push(str);
   }
 }
 
