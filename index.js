73c498a35d985959f3cb4377dabf4929d3f3539e
Mark Bello <mcbello51286@gmail.com>
2017-11-16 22:35:33 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,6 +1,23 @@
 function theBeatlesPlay(musicians, instruments){
   var arr = [];
   for(var i = 0; i< musicians.length; i++){
-    
+    arr.push(`${musicians[i]} plays ${instruments[i]}`);
   }
+}
+
+function johnLennonFacts(facts){
+  var counter = 0;
+  while(counter < facts.length){
+    facts[counter] += '!!!';
+    counter++;
+  }
+}
+
+function iLoveTheBeatles(n){
+  var arr = [];
+  var counter = 0;
+  do{
+    arr.push('I love the Beatles!');
+    counter++;
+  } while(counter < 15 && counter <= n);
 }
